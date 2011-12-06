49a84ba5b365a93bc0f846d5a225dd5c0fa0f390
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2011-12-06 17:58:14 -0500

    oops

--- config/initializers/scribd-carrierwave.rb
+++ config/initializers/scribd-carrierwave.rb
@@ -1,4 +1,4 @@
-ScribdCarrierWave.config.key = '5yspmefukl5soqzdpac9a'
-ScribdCarrierWave.config.secret = 'sec-2cbk8pkl80j3iora9jyn9m1yv'
-ScribdCarrierWave.config.username = 'aubrey_highgroove'
-ScribdCarrierWave.config.password = 'inlaw'
+ScribdCarrierWave.config.key = ''
+ScribdCarrierWave.config.secret = ''
+ScribdCarrierWave.config.username = ''
+ScribdCarrierWave.config.password = ''
